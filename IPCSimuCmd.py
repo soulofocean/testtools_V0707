a959f02aa51c70ffd3f310c4d969fae650b2b799
soulofocean <251455882@qq.com>
2018-07-09 16:24:13 +0800

    IPC剩余record_msg和stream没加，其他消息更新完成

--- IPCSimuCmd.py
+++ IPCSimuCmd.py
@@ -22,28 +22,6 @@ class IPCCmd(BasicCmd):
         self.do_start()
         self.onoff_kv = {"0": "off", "1": "on"}
 
-    def help_clean(self):
-        self.cprint.notice_p("clean the waterfilter:clean")
-
-    def do_clean(self, arg):
-        self.sim_obj.set_item('_status', 'clean')
-        self.sim_obj.task_obj.add_task(
-            'change WaterFilter to filter', self.sim_obj.set_item, 1, 100, '_status', 'standby')
-
-    def help_rsf(self):
-        self.cprint.notice_p("reset filter : rsf [id[1-2]] 0 for all")
-
-    def do_rsf(self, arg):
-        args = arg.split()
-        if (len(args) != 1 or not args[0].isdigit() or int(args[0]) > 2):
-            self.help_rsf()
-        else:
-            if (int(args[0]) == 0):
-                self.sim_obj.reset_filter_time(1)
-                self.sim_obj.reset_filter_time(2)
-            else:
-                self.sim_obj.reset_filter_time(int(args[0]))
-
 
 class IPC(BaseWifiSim):
     def __init__(self, logger, mac='123456', time_delay=500, self_addr=None, addr=('192.168.10.1', 65381)):
@@ -54,18 +32,49 @@ class IPC(BaseWifiSim):
         # self.sdk_obj.sim_obj = self
 
         # state data:
-        self._switch = 'off'
-        self._change = "off"
-        self._voice_offset = 27
+        self._pict_quality = 'general'
+        self._resolution = "general"
+        self._audio = 'off'
+        self._flip = 'off'
+        self._distortion_correction = 'off'
+        self._ir_switch = 'off'
+        self._osd = 'OSD_example'
+        self._mic = 'off'
+        self._encrypt_photo = 'off'
+        self._pwd = 'your_pwd'
+        self._record_method = 'motion_detect'
+        self._md_sensitivity = 'general'
+        self._cry = 'off'
+        self._time_zone = 'utc+8'
+        self._sdcard_msg = {
+            "status" : "ok",
+            "formate_result" : "success",
+            "total" : 1000,
+            "unused" : 100
+        }
+        self._voice = 'release'
 
 
     def get_event_report(self):
         report_msg = {
             "method": "report",
             "attribute": {
-                "switch": self._switch,
-                "change": self._change
-                # "_voice_offset": self._voice_offset,
+                "pict_quality": self._pict_quality,
+                "resolution": self._resolution,
+                "audio": self._audio,
+                "flip": self._flip,
+                "distortion_correction": self._distortion_correction,
+                "ir_switch": self._ir_switch,
+                "osd": self._osd,
+                "mic": self._mic,
+                "encrypt_photo": self._encrypt_photo,
+                "pwd": self._pwd,
+                "record_method": self._record_method,
+                "md_sensitivity": self._md_sensitivity,
+                "cry": self._cry,
+                "time_zone": self._time_zone,
+                "sdcard_msg": self._sdcard_msg,
+                "voice": self._voice
             }
         }
         return json.dumps(report_msg)
@@ -81,8 +90,22 @@ class IPC(BaseWifiSim):
                     "msg": "success",
                     "code": 0,
                     "attribute": {
-                        "switch": self._switch,
-                        "change": self._change
+                        "pict_quality": self._pict_quality,
+                        "resolution": self._resolution,
+                        "audio": self._audio,
+                        "flip": self._flip,
+                        "distortion_correction": self._distortion_correction,
+                        "ir_switch": self._ir_switch,
+                        "osd": self._osd,
+                        "mic": self._mic,
+                        "encrypt_photo": self._encrypt_photo,
+                        "pwd": self._pwd,
+                        "record_method": self._record_method,
+                        "md_sensitivity": self._md_sensitivity,
+                        "cry": self._cry,
+                        "time_zone": self._time_zone,
+                        "sdcard_msg": self._sdcard_msg,
+                        "voice": self._voice
                     }
                 }
                 return json.dumps(rsp_msg)
@@ -90,33 +113,104 @@ class IPC(BaseWifiSim):
                 self.LOG.warn('Unknow msg!')
 
         elif msg['method'] == 'dm_set':
-            if msg['nodeid'] == u"ipc.main.switch":
+            if msg['nodeid'] == u"ipc.main.pict_quality":
                 self.LOG.warn(
-                    ("设置开关/切换: %s" % (msg['params']["attribute"]["switch"])).encode(coding))
-                self.set_item('_switch', msg['params']["attribute"]["switch"])
+                    ("设置画质: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_pict_quality', msg['params']["attribute"]["value"])
                 return self.dm_set_rsp(msg['req_id'])
 
-            elif msg['nodeid'] == u"tv.main.change":
+            elif msg['nodeid'] == u"ipc.main.resolution":
                 self.LOG.warn(
-                    ("对讲控制: %s" % (msg['params']["attribute"]["change"])).encode(coding))
-                self.set_item('_switch', msg['params']["attribute"]["change"])
+                    ("清晰控制: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_resolution', msg['params']["attribute"]["value"])
                 return self.dm_set_rsp(msg['req_id'])
 
-            elif msg['nodeid'] == u"ipc.main.set":
+            elif msg['nodeid'] == u"ipc.main.audio":
                 self.LOG.warn(
-                    ("音量调整: %s" % (msg['params']["attribute"]["voice"])).encode(coding))
-                vof = int((msg['params']["attribute"]["voice"]).encode(coding))
-                tmpof = self.get_item("_voice_offset")
-                if(msg['params']["attribute"]["voice"] == u"increase"):
-                    self.set_item("_voice_offset", min(255, tmpof + vof))
-                elif (msg['params']["attribute"]["voice"] == u"decrease"):
-                    self.set_item("_voice_offset", max(0, tmpof - vof))
-                elif (msg['params']["attribute"]["voice"] == u"mute"):
-                    self.set_item("_voice_offset", 0)
-                else:
-                    self.LOG.error(
-                        ("unkown voice: %s" % (msg['params']["attribute"]["voice"])).encode(coding))
-                self.set_item('_switch', msg['params']["attribute"]["change"])
+                    ("语音ipc->app开关: %s" % (msg['params']["attribute"]["switch"])).encode(coding))
+                self.set_item('_audio', msg['params']["attribute"]["switch"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.flip":
+                self.LOG.warn(
+                    ("图像翻转: %s" % (msg['params']["attribute"]["switch"])).encode(coding))
+                self.set_item('_flip', msg['params']["attribute"]["switch"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.distortion_correction":
+                self.LOG.warn(
+                    ("畸变校正: %s" % (msg['params']["attribute"]["distortion_correction"])).encode(coding))
+                self.set_item('_distortion_correction', msg['params']["attribute"]["distortion_correction"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.cloud_platform":
+                self.LOG.warn(
+                    ("云台旋转: %s" % (msg['params']["attribute"]["direction"])).encode(coding))
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.ir_switch":
+                self.LOG.warn(
+                    ("红外夜视: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_ir_switch', msg['params']["attribute"]["value"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.osd":
+                self.LOG.warn(
+                    ("OSD: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_osd', msg['params']["attribute"]["value"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.mic":
+                self.LOG.warn(
+                    ("麦克风: %s" % (msg['params']["attribute"]["mic"])).encode(coding))
+                self.set_item('_mic', msg['params']["attribute"]["mic"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.encrypt_photo":
+                self.LOG.warn(
+                    ("图片加密: %s" % (msg['params']["attribute"]["encrypt_photo"])).encode(coding))
+                self.set_item('_encrypt_photo', msg['params']["attribute"]["encrypt_photo"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.pwd":
+                self.LOG.warn(
+                    ("密码设置: %s" % (msg['params']["attribute"]["pwd"])).encode(coding))
+                self.set_item('_pwd', msg['params']["attribute"]["pwd"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.record_method":
+                self.LOG.warn(
+                    ("移动侦测: %s" % (msg['params']["attribute"]["type"])).encode(coding))
+                self.set_item('_record_method', msg['params']["attribute"]["type"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.md_sensitivity":
+                self.LOG.warn(
+                    ("敏感度: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_md_sensitivity', msg['params']["attribute"]["value"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.cry":
+                self.LOG.warn(
+                    ("哭声侦测: %s" % (msg['params']["attribute"]["cry"])).encode(coding))
+                self.set_item('_cry', msg['params']["attribute"]["cry"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.time_zone":
+                self.LOG.warn(
+                    ("时区设置: %s" % (msg['params']["attribute"]["time_zone"])).encode(coding))
+                self.set_item('_time_zone', msg['params']["attribute"]["time_zone"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.sd_format":
+                self.LOG.warn(
+                    ("sd格式化: %s" % (msg['params']["attribute"]["value"])).encode(coding))
+                self.set_item('_sd_format', msg['params']["attribute"]["value"])
+                return self.dm_set_rsp(msg['req_id'])
+
+            elif msg['nodeid'] == u"ipc.main.set_time":
+                self.LOG.warn(
+                    ("设置时间: %s" % (msg['params']["attribute"]["value"])).encode(coding))
                 return self.dm_set_rsp(msg['req_id'])
 
             elif msg['nodeid'] == u"wifi.main.alarm_confirm":
