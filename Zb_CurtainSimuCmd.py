6537eeaea0d9e173102723b290b96c9964502e51
soulofocean <251455882@qq.com>
2018-07-17 17:38:10 +0800

    窗帘简单调试代码

--- Zb_CurtainSimuCmd.py
+++ Zb_CurtainSimuCmd.py
@@ -14,20 +14,19 @@ class CurtainCmd(BasicCmd):
     def __init__(self, logger, cprint):
         self.air_version = "20180703"
         self.mac = str(hex(int(time.time())))[-8:]
-        self.device_type = "Curtain_Wifi"
+        self.device_type = "Curtain"
         BasicCmd.__init__(self, logger=logger, cprint=cprint, version=self.air_version, d_type=self.device_type)
-        self.sim_obj = eval(self.device_type)(logger, mac=self.mac, addr=rout_addr)
-        self.do_start()
-        self.onoff_kv = {"0": "off", "1": "on", "2": "pause"}
 
 
 
 
 
 if __name__ == '__main__':
-    LOG = MyLogger(os.path.abspath(sys.argv[0]).replace('py', 'log'), clevel=logging.DEBUG,
+    LOG = MyLogger("%s.log" % (os.path.basename(sys.argv[0]).split(".")[0],), clevel=logging.INFO,
                    rlevel=logging.WARN)
     cprint = cprint(__name__)
     zigbee_obj = ZIGBEE('COM4', logger=LOG)
     zigbee_obj.run_forever()
     zigbee_obj.set_device(eval("Curtain"))
+    cmd = CurtainCmd(logger=LOG,cprint=cprint)
+    cmd.cmdloop()
