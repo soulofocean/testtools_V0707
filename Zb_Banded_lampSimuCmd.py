dc62923bacf2a941556e6aac8afa1dafd233fd70
soulofocean <251455882@qq.com>
2018-07-19 17:57:12 +0800

    新增灯带

---
+++ Zb_Banded_lampSimuCmd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'ZengXu'
+__mtime__ = '2018-7-19'
+"""
+from basic.BasicCommon import *
+from basic.BasicSimuCmd import BasicCmd,Banded_lamp,Shoot_lamp,Celling_lamp,Tube_lamp
+from basic.BasicProtocol import ZIGBEE
+
+
+class CurtainCmd(BasicCmd):
+    def __init__(self, logger, cprint):
+        self.air_version = "20180703"
+        self.mac = str(hex(int(time.time())))[-8:]
+        self.device_type = "Banded_lamp"
+        BasicCmd.__init__(self, logger=logger, cprint=cprint, version=self.air_version, d_type=self.device_type)
+
+
+
+
+
+
+if __name__ == '__main__':
+    LOG = MyLogger("%s.log" % (os.path.basename(sys.argv[0]).split(".")[0],), clevel=logging.INFO,
+                   rlevel=logging.WARN)
+    cprint = cprint(__name__)
+    zigbee_obj = ZIGBEE('COM4', logger=LOG)
+    zigbee_obj.run_forever()
+    zigbee_obj.set_device(eval("Banded_lamp"))
+    cmd = CurtainCmd(logger=LOG,cprint=cprint)
+    cmd.cmdloop()
