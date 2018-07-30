e092322169f5b2bc0e3502cb55cb48f3ef348ad2
soulofocean <251455882@qq.com>
2018-07-30 09:40:23 +0800

    四种灯支持配置文件

--- Zb_Tube_lampSimuCmd.py
+++ Zb_Tube_lampSimuCmd.py
@@ -8,6 +8,7 @@ __mtime__ = '2018-7-19'
 from basic.BasicCommon import *
 from basic.BasicSimuCmd import BasicCmd,Tube_lamp
 from basic.BasicProtocol import ZIGBEE
+import ConfigParser
 
 
 class Tube_lampCmd(BasicCmd):
@@ -23,10 +24,18 @@ class Tube_lampCmd(BasicCmd):
 
 
 if __name__ == '__main__':
-    LOG = MyLogger("%s.log" % (os.path.basename(sys.argv[0]).split(".")[0],), clevel=logging.INFO,
-                   rlevel=logging.WARN)
+    cf = ConfigParser.ConfigParser()
+    cf.read('zigbee_devices.conf')
+    port = cf.get('Common', 'port')
+    cl_level = eval(cf.get('Common', 'cl_level'))
+    fl_level = eval(cf.get('Common', 'fl_level'))
+    rm_log = eval(cf.get('Common', 'rm_log'))
+    logpath = os.path.abspath(sys.argv[0]).replace('py', 'log').replace('exe', 'log')
+    if rm_log and os.path.isfile(logpath):
+        os.remove(logpath)
+    LOG = MyLogger(logpath, clevel=cl_level, flevel=fl_level)
     cprint = cprint(__name__)
-    zigbee_obj = ZIGBEE('COM4', logger=LOG)
+    zigbee_obj = ZIGBEE(port, logger=LOG)
     zigbee_obj.run_forever()
     zigbee_obj.set_device(eval("Tube_lamp"))
     cmd = Tube_lampCmd(logger=LOG, cprint=cprint)
